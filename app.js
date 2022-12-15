a2cec30ec061dd70ec29155b752eb0558951ee9c
Genesis Thomas <genesisthomasit@gmail.com>
2022-12-15 18:17:05 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const { bootstrap } = require("@kaholo/plugin-library");
+
+async function hello(params) {
+  const {
+    helloName,
+    saySecret,
+    secret,
+  } = params;
+
+  let greeting = `Hello ${helloName}!`;
+
+  if (saySecret && !secret) {
+    throw new Error("No secret was provided to say. Please provide a secret or uncheck \"Say Secret\".");
+  }
+
+  if (saySecret) {
+    greeting += `\nHere is the secret: ${secret}`;
+  }
+
+  return greeting;
+}
+
+module.exports = bootstrap({
+  hello,
+});
